a2bef7cd41360ff1e8c3a9d765c299df1a6df54e
Bjnstad <axebjo@gmail.com>
2018-02-06 10:14:46 +0100

    added fxml example

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+/*
+ * Opprett JavaFX GUI program. Bruk FXMLLoader for � laste inn FXML
+ * som beskriver GUI.
+ */
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+	       
+		Scene scene = new Scene(root, 800, 600);
+	    
+		primaryStage.setTitle("FXML eksempel, med grafikk");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
