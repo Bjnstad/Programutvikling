7c605983c2a24fb294ca477b9750e2caefaa4ee4
Henry Tran <Henry.nor@gmail.com>
2018-02-15 12:32:49 +0100

    edit syntax

--- src/game/GameState.java
+++ src/game/GameState.java
@@ -5,12 +5,14 @@ package game;
  */
 public abstract class GameState {
     private State state;
+
     public GameState(State state){
         this.state = state;
     }
     public abstract void initiate();
     public abstract void render();
     public abstract void onClose();
+
     public  State getState() {
         return this.state;
     }
