edd3567c3eb5ed22c5bb9bf188572088c2b00b1a
Henry Tran <Henry.nor@gmail.com>
2018-05-16 07:43:04 +0200

    Export mapTest

---
+++ tests/hac/model/filehandler/ExportMapTest.java
@@ -0,0 +1,93 @@
+package hac.model.filehandler;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by henrytran1 on 16/05/2018.
+ */
+class ExportMapTest {
+
+    private ExportMap exportMap;
+
+
+    @BeforeEach
+    void setUp() {
+        exportMap = new ExportMap();
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void addElement() {
+
+    }
+
+    @Test
+    void addMapSize() {
+
+    }
+
+    @Test
+    void saveGame() {
+
+    }
+
+    @Test
+    void saveObjects() {
+
+    }
+
+    @Test
+    void appendAhac() {
+
+    }
+
+    @Test
+    void saveGameState() {
+
+    }
+
+    @Test
+    void handleSaveMapName() {
+
+    }
+
+    @Test
+    void getElements() {
+
+    }
+
+    @Test
+    void encodeImageToString() {
+
+    }
+
+    @Test
+    void encodeStringToImage() {
+
+    }
+
+    @Test
+    void createFile() {
+
+    }
+
+    @Test
+    void saveSpriteInput() {
+
+    }
+
+    @Test
+    void showAlert() {
+
+    }
+
+}
